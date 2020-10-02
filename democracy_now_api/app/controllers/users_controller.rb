009a7aab3e81ba5795233244b383157eec668ad6
Justin <justinms@outlook.com>
2020-10-02 15:23:15 -0500

    allow for the creation of a user

---
+++ democracy_now_api/app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+    def create
+        user = User.new(user_params)
+
+        if user.save
+            session[:user_id] = user.id
+            render json: {
+                status: 201,
+                logged_in: true,
+                user: user
+            }
+        else
+            render json: {
+                status: 500,
+                error: 'User could not be created'
+            }
+        end 
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :street_address, :city, :state, :zip_code, :country)
+    end
+end 
