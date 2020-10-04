5e13f504b501427e0ae03c051fe65c954c086b9e
Justin <justinms@outlook.com>
2020-10-03 21:37:10 -0500

    handle user login and logout

--- democracy_now_client/src/components/Home.js
+++ democracy_now_client/src/components/Home.js
@@ -1,13 +1,41 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Login from './auth/Login'
 import Registration from './auth/Registration'
 
-function Home() {
-    return(
-        <div>
-            <h1>Home Page</h1>
-            <Registration />
-        </div>
-    )
+class Home extends Component {
+    
+    handleAuth = (userData) => {
+        this.props.handleLogin(userData)
+
+        // How we redirect
+        this.props.history.push("/dashboard")
+    }
+
+    handleLogoutClick = () => {
+        this.props.handleLogout()
+
+        fetch("http://localhost:3001/logout", {credentials: "include"})
+        .then(response => {
+            this.props.handleLogout()
+        })
+        .catch(error => {
+            console.log("Logout Error", error)
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                <h1>Home Page</h1>
+                <h1>Status: {this.props.loggedInStatus}</h1>
+                <Registration handleAuth={this.handleAuth}/>
+                <hr></hr>
+                <Login handleAuth={this.handleAuth}/>
+                <hr></hr>
+                <button onClick={this.handleLogoutClick}>Log Out</button>
+            </div>
+        )
+    }
 }
 
 export default Home
