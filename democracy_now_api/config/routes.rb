8484efc2136226e8c66e40d077d1e0f1ec303a40
Justin <justinms@outlook.com>
2020-10-03 21:39:26 -0500

    add route to check login status

--- democracy_now_api/config/routes.rb
+++ democracy_now_api/config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   
   resources :sessions, only: [:create]
-  get '/logout', to: 'sessions#logout', as: 'logout'
+  get '/logout', to: 'sessions#logout'
+  get '/current_login', to: 'sessions#current_login'
 
   resources :users, only: [:create]
 end
