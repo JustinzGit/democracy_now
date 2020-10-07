545e1249a6e1aa9fce0ac848447c55e45d7d1dd6
Justin <justinms@outlook.com>
2020-10-07 13:11:34 -0500

    handle user login

--- democracy_now_client/src/components/Home.js
+++ democracy_now_client/src/components/Home.js
@@ -1,38 +1,41 @@
 import React, { Component } from 'react'
-import Login from './auth/Login'
-import Registration from './auth/Registration'
+// import { Redirect } from 'react-router-dom'
+// import { Redirect } from 'react-router-dom'
+// import Login from './auth/Login'
+// import Signup from './auth/Signup'
 
 class Home extends Component {
     
-    handleAuth = (userData) => {
-        this.props.handleLogin(userData)
-
-        // How we redirect
-        this.props.history.push("/dashboard")
-    }
+    // handleAuth = (userData) => {
+    //     this.props.handleLogin(userData)
+    //     this.props.history.push("/dashboard")
+    // }
 
     handleLogoutClick = () => {
-        this.props.handleLogout()
+        // this.props.handleLogout()
 
         fetch("http://localhost:3001/logout", {credentials: "include"})
         .then(response => {
             this.props.handleLogout()
+            this.props.history.push("/login")
         })
         .catch(error => {
             console.log("Logout Error", error)
         })
     }
 
+    handleLoginClick = () => {
+        this.props.history.push("/login")
+    }
+
     render(){
+        console.log("HOME = HOME WAS HIT")
+        console.log("HOME = Logged In Status:", this.props.loggedInStatus)
         return(
             <div>
                 <h1>Home Page</h1>
-                <h1>Status: {this.props.loggedInStatus}</h1>
-                <Registration handleAuth={this.handleAuth}/>
-                <hr></hr>
-                <Login handleAuth={this.handleAuth}/>
-                <hr></hr>
-                <button onClick={this.handleLogoutClick}>Log Out</button>
+                <h3>{this.props.loggedInStatus ? "Logged In" : "Not Logged In"}</h3>
+                {this.props.loggedInStatus ? <button onClick={this.handleLogoutClick}>Log Out</button> : <button onClick={this.handleLoginClick}>Log In</button>}
             </div>
         )
     }
