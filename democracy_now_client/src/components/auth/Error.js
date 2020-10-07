0a773ef5524921cbacde406f00504cf7b19d9bd9
Justin <justinms@outlook.com>
2020-10-07 15:56:54 -0500

    add key to list items

--- democracy_now_client/src/components/auth/Error.js
+++ democracy_now_client/src/components/auth/Error.js
@@ -4,7 +4,7 @@ const Error = ({messages}) => {
     return (
         <div>
             <h3>Error!</h3>
-            <ul>{messages.map(message => <li>{message}</li>)}</ul>
+            <ul>{messages.map((message, index) => <li key={index}>{message}</li>)}</ul>
         </div>
     )
 }
