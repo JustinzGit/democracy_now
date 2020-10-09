31cf9e8a7a8db5c495405d0a7f8717cfd6d742c0
Justin <justinms@outlook.com>
2020-10-09 18:03:10 -0500

    use redux to manage state

--- nasa_now_client/src/index.js
+++ nasa_now_client/src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+import userAuth from './reducers/userAuth'
+
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'redux-thunk' 
+
+const store = createStore(userAuth, applyMiddleware(thunk))
 
 ReactDOM.render(
-      <App />,
+      <Provider store={store}>
+        <App />
+      </Provider>,
   document.getElementById('root')
 );
