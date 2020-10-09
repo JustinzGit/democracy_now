a2ea39340bcfdc2de1d6965f1b269615cc8ea292
Justin <justinms@outlook.com>
2020-10-09 18:35:54 -0500

    convert class component to function

--- nasa_now_client/src/components/Home.js
+++ nasa_now_client/src/components/Home.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { Redirect } from 'react-router-dom'
 
-class Home extends Component {
 
-    handleLogoutClick = () => {
-        this.props.handleLogout()
-    }
+const Home = ({ handleLogout }) => {
 
-    render(){
-        return(
-            <div>
-                <h1>Home Page</h1>
-                <p><button onClick={() => this.props.history.push('/apod') }>Astronomy Picture of the Day</button></p>
-                <p><button onClick={() => this.props.history.push('/asteroids') }>Discover Asteroids</button></p>
-                <p><button onClick={this.handleLogoutClick}>Log Out</button></p>
-            </div>
-        )
-    }
+    return(
+        <div>
+            <h1>Home Page</h1>
+            <p><button onClick={() => <Redirect to='/apod' />}>Astronomy Picture of the Day</button></p>
+            <p><button onClick={() => this.props.history.push('/asteroids') }>Discover Asteroids</button></p>
+            <p><button onClick={handleLogout}>Log Out</button></p>
+        </div>
+    )
 }
 
 export default Home
