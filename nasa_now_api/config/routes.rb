3d92d0d25b5f80a5d46c0190e3daf5c1558a1d0c
Justin <justinms@outlook.com>
2020-10-08 23:48:46 -0500

    setup route to fetch nasas apod

--- nasa_now_api/config/routes.rb
+++ nasa_now_api/config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   
+  # Session Routes
   resources :sessions, only: [:create]
   get '/logout', to: 'sessions#logout'
   get '/current_login', to: 'sessions#current_login'
 
+  # User Routes
   resources :users, only: [:create]
+
+  # NASA Routes
+  get 'nasa/apod/(:date)', to: 'nasa#apod'
 end
