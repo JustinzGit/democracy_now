bf9bbb09c3381503da308cda9d3389c15e652eac
Justin <justinms@outlook.com>
2020-10-10 14:23:44 -0500

    pass in default param

--- nasa_now_client/src/actions/nasa/fetchApod.js
+++ nasa_now_client/src/actions/nasa/fetchApod.js
@@ -1,19 +1,19 @@
-export default function fetchApod(dateInput){
+export default function fetchApod(dateInput=""){
     return (dispatch) => {
         dispatch({ type: 'START_APOD_REQUEST'})
         fetch(`http://localhost:3001/nasa/apod/${dateInput}`)
         .then(response => response.json())
         .then(apiData => {
-            if (apiData.staus === 200){
+            if (apiData.status === 200){
                 return dispatch({ 
                     type: 'ACQUIRE_APOD', 
-                
-                    astronomyPic: {
-                        date: apiData.date,
-                        title: apiData.title,
-                        hdurl: apiData.hdurl,
-                        explanation: apiData.explanation
-                    },})
+                    
+                    astronomyPic: { 
+                        date: apiData.date, 
+                        title: apiData.title, 
+                        hdurl: apiData.hdurl, 
+                        explanation: apiData.explanation 
+                    }})
             }
         })
     }
