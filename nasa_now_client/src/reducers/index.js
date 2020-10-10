4690740ad1dfd1592eea7eb847d6f6b4ba02c377
Justin <justinms@outlook.com>
2020-10-10 10:09:38 -0500

    combine reducers

---
+++ nasa_now_client/src/reducers/index.js
@@ -0,0 +1,6 @@
+import { combineReducers } from 'redux'
+import userAuth from './userAuth'
+import Nasa from './Nasa'
+import userAuth from './userAuth'
+
+export default combineReducers({ Nasa, userAuth })
