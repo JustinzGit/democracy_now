e41a0dc3480e7689f8952312c6d321ea3b99f786
Justin <justinms@outlook.com>
2020-10-09 21:00:38 -0500

    remove unneded buttons

--- nasa_now_client/src/components/Home.js
+++ nasa_now_client/src/components/Home.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { Redirect } from 'react-router-dom'
+
 
 
 const Home = ({ handleLogout }) => {
-
+    
     return(
         <div>
             <h1>Home Page</h1>
-            <p><button onClick={() => <Redirect to='/apod' />}>Astronomy Picture of the Day</button></p>
-            <p><button onClick={() => this.props.history.push('/asteroids') }>Discover Asteroids</button></p>
             <p><button onClick={handleLogout}>Log Out</button></p>
         </div>
     )
