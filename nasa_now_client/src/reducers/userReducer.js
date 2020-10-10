b008c17f17190791cc75a598bf76e4c2f2f16f4d
Justin <justinms@outlook.com>
2020-10-10 11:35:01 -0500

    renamed reducers

---
+++ nasa_now_client/src/reducers/userReducer.js
@@ -0,0 +1,75 @@
+export default function userReducer(
+    state = {
+        requestingData: false,
+        loggedInStatus: false, 
+        user: {}
+    }, 
+    action){
+
+    switch (action.type){
+      
+        case 'START_LOGIN_REQUEST':
+            return {
+                ...state, 
+                user: {...state.user}, 
+                requestingData: true
+            }
+
+        case 'LOGIN':
+            console.log("CASE - WAS HIT")
+            return {
+                ...state, 
+                user: action.user, 
+                requestingData: false,
+                loggedInStatus: true
+            }
+
+        case 'START_LOGOUT_REQUEST':
+            return {
+                ...state, 
+                user: {...state.user}, 
+                requestingData: true
+            }
+
+        case 'LOGOUT':
+            return {
+                ...state,
+                user: {},
+                requestingData: false,
+                loggedInStatus: false
+            }
+
+        case 'START_SIGNUP_REQUEST':
+            return {
+                ...state,
+                user: {...state.user},
+                requestingData: true
+            }
+            
+        case 'SIGNUP':
+            return {
+                ...state,
+                user: action.user,
+                requestingData: false,
+                loggedInStatus: true
+            }
+
+        case 'ACQUIRING_CURRENT_LOGIN':
+            return {
+                ...state,
+                user: {...state.user},
+                requestingData: true
+            }
+
+        case 'CURRENT_LOGIN':
+            return {
+                ...state,
+                user: action.user,
+                requestingData: false,
+                loggedInStatus: action.loggedInStatus
+            }
+
+        default:
+            return state
+    }
+}
