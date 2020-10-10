91c2fe1b00055f7084af59474f8ef8a7de3606dd
Justin <justinms@outlook.com>
2020-10-10 10:09:54 -0500

    use combined reducer

--- nasa_now_client/src/index.js
+++ nasa_now_client/src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App';
-import userAuth from './reducers/userAuth'
+import reducer from './reducers/index'
 
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk' 
 
-const store = createStore(userAuth, applyMiddleware(thunk))
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
       <Provider store={store}>
