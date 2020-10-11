10cead0ce59ad0958b1082c3b01cc693494fe84f
Justin <justinms@outlook.com>
2020-10-10 19:32:03 -0500

    create container for data management

---
+++ nasa_now_client/src/containers/nasa/AsteroidsContainer.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+import { DateTime } from "luxon"
+import { connect } from 'react-redux'
+
+import fetchAsteroids from '../../actions/nasa/fetchAsteroids'
+import Asteroids from '../../components/nasa/Asteroids'
+
+class AsteroidsContainer extends Component {    
+    componentDidMount(){
+        this.props.fetchAsteroids()
+    }
+
+    setDate = (event) => {
+        this.setState({ astroidDate: event.target.value })
+    }
+
+    render(){
+        if(!this.props.requestingData){
+            return(
+                <div>
+                    <Asteroids asteroids={this.props.asteroids} />
+                </div>
+            )
+        }
+        else{
+            return(<div><h1>Loading Asteroid Data</h1></div>)
+        }
+        
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        requestingData: state.nasa.requestingData,
+        asteroids: state.nasa.asteroids
+    }
+}
+
+export default connect(mapStateToProps, { fetchAsteroids })(AsteroidsContainer)
