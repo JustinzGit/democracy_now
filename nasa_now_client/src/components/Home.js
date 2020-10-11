3d36e67d2b750bebf839ccbe79559f11b647e20a
Justin <justinms@outlook.com>
2020-10-11 09:25:54 -0500

    move nasa logo into seperate component

--- nasa_now_client/src/components/Home.js
+++ nasa_now_client/src/components/Home.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-import Logo from '../nasa-logo.png'
+import NasaLogo from './NasaLogo'
 import LogoutButton from './LogoutButton'
 
 const Home = () => {
     return(
         <div id="home">
-            <img src={Logo} alt="nasa_logo" style={{maxWidth: "100%", width: 800, height: 'auto'}}></img>
             <div>
-                <h1>NASA NOW</h1>
+                <NasaLogo />              
                 <Link to='/apod'>Astronomy Picture of the Day</Link>
                 <Link to='/asteroids'>Today's Asteroids</Link>
                 <LogoutButton />
