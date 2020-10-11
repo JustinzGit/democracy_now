7e4d6b9887613aa076b388bae07002bf8c264cc7
Justin <justinms@outlook.com>
2020-10-10 20:36:50 -0500

    import css

--- nasa_now_client/src/index.js
+++ nasa_now_client/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router } from 'react-router-dom'
+
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import App from './App';
 import reducer from './reducers/rootReducer'
