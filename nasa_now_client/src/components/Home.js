e1c6a1700d581a6bfe4ccb7d2a5bfeed147b70c5
Justin <justinms@outlook.com>
2020-10-10 22:52:27 -0500

    modify styling

--- nasa_now_client/src/components/Home.js
+++ nasa_now_client/src/components/Home.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import Logo from '../nasa-logo.png'
+
 const Home = () => {
     return(
-        <div>
-            <h1>Home Page</h1>
-            <Link to='/apod'>Astronomy Picture of the Day</Link><br></br>
-            <Link to='/asteroids'>Today's Asteroids</Link>
+        <div id="home">
+            <img src={Logo} alt="nasa_logo" style={{maxWidth: "100%", width: 800, height: 'auto'}}></img>
+            <div>
+                <h1>NASA NOW</h1>
+                <Link to='/apod'>Astronomy Picture of the Day</Link>
+                <Link to='/asteroids'>Today's Asteroids</Link>
+            </div>
         </div>
     )
 }
