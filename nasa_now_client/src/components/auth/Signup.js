b80b243ad65aeb68299e7bc941925ddfc28c23f8
Justin <justinms@outlook.com>
2020-10-11 09:37:31 -0500

    apply bootstrap styling to form, add nasa logo

--- nasa_now_client/src/components/auth/Signup.js
+++ nasa_now_client/src/components/auth/Signup.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react"
 import { connect } from 'react-redux'
 import { Redirect } from "react-router-dom"
-import handleSignup from '../../actions/auth/handleSignup'
+
 // import Error from './Error'
+import handleSignup from '../../actions/auth/handleSignup'
+import { Form, Button } from 'react-bootstrap'
+import NasaLogo from '../NasaLogo'
 
 class Signup extends Component {
 
@@ -32,24 +35,24 @@ class Signup extends Component {
         }
 
         return(
-            <div>
+            <div class="form">
                 {/* {this.state.error && <Error messages={this.state.error} /> } */}
+                <NasaLogo />
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="text" onChange={this.handleChange} name="email" value={this.state.email}/>
 
-                <form onSubmit={this.handleSubmit}>
-                    <p>Email:
-                    <input type="text" onChange={this.handleChange} name="email" value={this.state.email}></input></p>
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="text" onChange={this.handleChange} name="password" value={this.state.password}/>
 
-                    <p>Password:
-                    <input type="text" onChange={this.handleChange} name="password" value={this.state.password}></input></p>
+                    <Form.Label>Password Confirmation</Form.Label>
+                    <Form.Control  type="text" onChange={this.handleChange} name="password_confirmation" value={this.state.password_confirmation}/>
 
-                    <p>Password Confirmation:
-                    <input type="text" onChange={this.handleChange} name="password_confirmation" value={this.state.password_confirmation}></input></p>
-
-                    <input type="submit" value="SignUp"></input>
+                    <Button id="signup_btn" variant="primary" type="submit">Sign Up</Button>
 
                     <p>Already Have An Account?</p>
-                    <button onClick={this.handleOnClick}>Login</button>
-                </form>
+                    <Button onClick={this.handleOnClick}>Login</Button>
+                </ Form>
             </div>
         )
     }
