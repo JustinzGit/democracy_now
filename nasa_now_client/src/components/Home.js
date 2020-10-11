6fba3790d6cbc26f25f0c9ed4afa63572d453cb3
Justin <justinms@outlook.com>
2020-10-10 20:48:04 -0500

    add links to components

--- nasa_now_client/src/components/Home.js
+++ nasa_now_client/src/components/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 const Home = () => {
     return(
         <div>
             <h1>Home Page</h1>
+            <Link to='/apod'>Astronomy Picture of the Day</Link><br></br>
+            <Link to='/asteroids'>Today's Asteroids</Link>
         </div>
     )
 }
