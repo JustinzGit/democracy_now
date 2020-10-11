2fb44a9d98f6b99be59fb31c966e08fe31e16f7a
Justin <justinms@outlook.com>
2020-10-10 19:27:47 -0500

    create action to fetch asteroid data

---
+++ nasa_now_client/src/actions/nasa/fetchAsteroids.js
@@ -0,0 +1,17 @@
+export default function fetchAsteroids(){
+    return (dispatch) => {
+        dispatch({ type: 'START_ASTEROIDS_REQUEST'})
+        fetch(`http://localhost:3001/nasa/asteroids/`)
+        .then(response => response.json())
+        .then(apiData => {
+            if (apiData.status === 200){
+
+                let dateArray = Object.keys(apiData.near_earth_objects).sort()
+                let currentAsteroids = apiData.near_earth_objects[dateArray[0]]
+                
+                return dispatch({ type: 'ACQUIRE_ASTEROIDS', currentAsteroids })
+            }
+
+        })
+    }
+}
