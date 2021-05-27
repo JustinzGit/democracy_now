31a57bbf3536a0b54aea9c73173a7c574bdb0758
Justin <justinms@outlook.com>
2021-05-27 08:53:56 -0500

    renamed heroku app, change origin

--- nasa_now_api/config/initializers/cors.rb
+++ nasa_now_api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://nasa-now-frontend.herokuapp.com'
+        origins 'https://nasa-now.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true 
     end  
 end 
