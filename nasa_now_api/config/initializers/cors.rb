2ab6337e505d8a76354f980ee1e9a31ea1904753
Justin <justinms@outlook.com>
2021-05-26 20:07:57 -0500

    change origin to heroku url

--- nasa_now_api/config/initializers/cors.rb
+++ nasa_now_api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'http://localhost:3000'
+        origins 'https://nasa_now.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true 
     end  
 end 
