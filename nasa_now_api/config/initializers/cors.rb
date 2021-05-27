1abddc71ab98d49e1d4ba9096ce010aa34cf6486
Justin <justinms@outlook.com>
2021-05-26 21:16:43 -0500

    use correct heroku origin

--- nasa_now_api/config/initializers/cors.rb
+++ nasa_now_api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins 'https://nasa_now.herokuapp.com'
+        origins 'https://nasa-now-frontend.herokuapp.com'
         resource '*', headers: :any, methods: [:get, :post, :patch, :put, :delete], credentials: true 
     end  
 end 
